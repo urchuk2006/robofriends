6bdc661840e68927cc52950008301b03d152d00b
Yuriy Lisik <urchuk2006@gmail.com>
2022-01-04 11:33:55 +0200

    first commit

---
+++ src/components/CardList.js
@@ -0,0 +1,27 @@
+import React, { Fragment }  from 'react';
+import Card from './Card';
+
+
+const CardList = ({ robots }) => {
+	// if (true) {
+	// 	throw new Error('NOOOO!');
+	// }
+	return (
+	<Fragment>
+		{
+			robots.map((user, i) => {
+				return (
+					<Card 
+						key={i} 
+						id={robots[i].id} 
+						name={robots[i].name} 
+						email={robots[i].email} 
+					/>
+				
+				);
+		})}
+	</Fragment>
+		);
+}
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import Card from './Card';
+import App from './containers/App';
+import 'tachyons';
+//import { robots } from './robots';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+ 	<App />
   </React.StrictMode>,
   document.getElementById('root')
 );
